d03e5e115645f15fbf63c20908b9fd20d3ad7646
Acidrain9 <chaoticf8@gmail.com>
2022-02-11 09:35:57 +0500

    Adding Multiuser Functionality (WIP)

--- logins.py
+++ logins.py
@@ -1,8 +1,13 @@
 # Login No.1 Account: Arbaz
 # Passwords are encoded
 login1 = "19003105002"
-pass1 = "VW10QDIwMjE="
+pass1 = 'VW10QDIwMjE='
 
 course1 = "https://sktlms.umt.edu.pk/moodle/course/view.php?id=5428"
+
+
+students = ["Arbaz", "Saad", "Hira"]
+ids = ["19003105002", "19003105009", "19003105006"]
+paswds = ["VW10QDIwMjE", "WHl6YkA5MTI5eHl6", ""]
 
 

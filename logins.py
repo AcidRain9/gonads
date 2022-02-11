3fdf36252f2d49cf6428f293a01e8236151a43f1
Acidrain9 <chaoticf8@gmail.com>
2022-02-12 01:50:45 +0500

    Saving Assignment links in database

--- logins.py
+++ logins.py
@@ -1,4 +1,3 @@
-# Login No.1 Account: Arbaz
 # Passwords are encoded
 
 course1 = "https://sktlms.umt.edu.pk/moodle/course/view.php?id=5428"
@@ -6,6 +5,6 @@ course1 = "https://sktlms.umt.edu.pk/moodle/course/view.php?id=5428"
 
 students = ["Arbaz", "Saad", "Hira"]
 ids = ["19003105002", "19003105009", "19003105006"]
-paswds = ["VW10QDIwMjE", "WHl6YkA5MTI5eHl6", ""]
+paswds = ["", "", ""]
 
 

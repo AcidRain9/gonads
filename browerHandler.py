7f24e2f2e9f6e358689be45a5c24fa36e7bbc39d
Acidrain9 <chaoticf8@gmail.com>
2022-02-12 07:38:50 +0500

    Refactoring Code

---
+++ browerHandler.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import logins as ac
+from bs4 import BeautifulSoup
+import re
+
+
+class SetupBrowser:
+
+    # Setting up Google Chrome/Chromium Instance
+    def __init__(self):
+        self.op = Options()
+        self.op.add_argument("start-maximized")
+        self.op.add_argument("--incognito")
+        self.op.add_experimental_option("excludeSwitches", ['enable-automation'])
+        prefs = {
+            'profile.managed_default_content_settings.images': 2,
+            'profile.managed_default_content_settings.javascript': 2
+        }
+        self.op.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(options=self.op)
+
+    def login(self, studentid, passwords):
+        # Finding Login Fields
+        self.driver.get("https://sktlms.umt.edu.pk/")
+        _id = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@id='username']")))
+        _pass = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@id='password']")))
+        _loginbtn = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@id='loginbtn']")))
+        # Passing login data
+        _id.send_keys(studentid)
+        _pass.send_keys(passwords)
+        _loginbtn.click()
+
+    # fetch links
+    def fetch_links(self):
+        # load course
+        self.driver.get(ac.course1)
+        # Finding Assignment  Links
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        assignments = soup.find_all('a', href=re.compile("https://sktlms\.umt\.edu\.pk/moodle/mod/assign/view\.php\?id="))
+        return assignments
+
+    def visit(self, link):
+        self.driver.get(link)
--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from bs4 import BeautifulSoup
-import re
 import logins as ac
 import base64
 import sqlite3
+from browerHandler import SetupBrowser
 
-# Setting up Google Chrome/Chromium Instance
-op = Options()
-op.add_argument("start-maximized")
-op.add_argument("--incognito")
-op.add_experimental_option("excludeSwitches", ['enable-automation'])
-prefs = {
-    'profile.managed_default_content_settings.images': 2,
-    'profile.managed_default_content_settings.javascript': 2
-}
-op.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(options=op)
-
-# Finding Login Fields
-driver.get("https://sktlms.umt.edu.pk/")
-_id = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@id='username']")))
-_pass = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@id='password']")))
-_loginbtn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='loginbtn']")))
-
-# Passing login data
-_id.send_keys(ac.ids[0])
-_pass.send_keys(base64.b64decode(ac.paswds[0]).decode("utf-8"))
-_loginbtn.click()
-
-# Loading course
-driver.get(ac.course1)
-
-# Finding Assignment  Links
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-assignments = soup.find_all('a', href=re.compile("https://sktlms\.umt\.edu\.pk/moodle/mod/assign/view\.php\?id="))
+browser = SetupBrowser()
+browser.login(ac.ids[0], base64.b64decode(ac.paswds[0]).decode("utf-8"))
+assignments = browser.fetch_links()
 
 # Connect to database
 conn = sqlite3.connect('assignments1.db')
@@ -70,7 +38,7 @@ for student in ac.students:
     print(student)
     for row in result:
         if row[1] == 0:
-            driver.get(row[0])
+            browser.visit(row[0])
             tmp2 = "UPDATE test SET {0} = 1 WHERE Assignments = '{1}'"
             query2 = tmp2.format(student, row[0])
             conn.execute(query2)
